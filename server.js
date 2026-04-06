7ba22ec7b39197f81641e5c1fc98b5221d7a2888
unknown <zhafrankuncoro@gmail.com>
2026-04-06 22:08:39 +0700

    update: connect to frontend

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
